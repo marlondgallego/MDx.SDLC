8148f07ab32e0b90abf72763281bc1d40a9aac98
Marlon D. Gallego <marlon.gallego@abbott.com>
2026-08-14 22:11:01 -0700

    Initial Commit

---
+++ SDLCTests/SDLCTests.swift
@@ -0,0 +1,19 @@
+//
+//  SDLCTests.swift
+//  SDLCTests
+//
+//  Created by rentamac on 8/14/26.
+//
+
+import Testing
+@testable import SDLC
+
+struct SDLCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
